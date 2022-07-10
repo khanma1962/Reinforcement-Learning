c4c934141c6251066f866e9a228f8c22802d2fb7
Moe Khan <m_a_khan_usa@yahoo.com>
2022-07-10 15:33:12 -0700

    Not working....

---
+++ Epsilon Greedy.py
@@ -0,0 +1,23 @@
+# This will implement Epsilon Greedy 
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+class Bandit():
+    def __init__(self, p):
+        self.p = p
+        self_estimate = 
+        self.N = 1000
+
+    def pull():
+        pass
+
+    def update():
+        pass
+
+
+    
+
+
